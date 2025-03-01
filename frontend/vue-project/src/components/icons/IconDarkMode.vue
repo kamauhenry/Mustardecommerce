491578c3cdb8609836478e757e6e6624f4f1dbaa
Allan <allancheboiw0@gmail.com>
2025-03-01 11:00:58 +0800

    deleting unnecessary files

--- frontend/vue-project/src/components/icons/IconDarkMode.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <svg class="feather feather-moon" fill="none" stroke="#838636" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-</template>
-<style scoped>
-  svg {
-    width: 2rem;
-    height: auto;
-  }
-</style>
