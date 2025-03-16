6161bed3583df695efc02bf62d9bc176933761eb
Allan <allancheboiw0@gmail.com>
2025-03-16 18:58:01 +0800

    UI updates (issue: products duplicate every page refresh)

--- frontend/vue-project/src/views/ProductDetails.vue
+++ frontend/vue-project/src/views/ProductDetails.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>
-    <h1>All Categories</h1>
-    <div v-if="store.loading.allCategoriesWithProducts">Loading...</div>
-    <div v-else-if="store.error.allCategoriesWithProducts">{{ store.error.allCategoriesWithProducts }}</div>
-    <ul v-else>
-      <li v-for="category in store.allCategoriesWithProducts" :key="category.id">
-        <router-link :to="`/category/${category.slug}`">{{ category.name }}</router-link>
-      </li>
-    </ul>
-  </div>
+  <MainLayout>
+    <div>
+      <h1>All Categories</h1>
+      <div v-if="store.loading.allCategoriesWithProducts">Loading...</div>
+      <div v-else-if="store.error.allCategoriesWithProducts">{{ store.error.allCategoriesWithProducts }}</div>
+      <ul v-else>
+        <li v-for="category in store.allCategoriesWithProducts" :key="category.id">
+          <router-link :to="`/category/${category.slug}`">{{ category.name }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </MainLayout>
 </template>
 
 <script>
 import { onMounted, computed } from 'vue';
 import { useEcommerceStore } from '@/stores/ecommerce';
+import MainLayout from '../components/navigation/MainLayout.vue';
 
 export default {
   props: { categorySlug: String, productSlug: String },
+  components: {
+    MainLayout,
+  },
   setup(props) {
     const store = useEcommerceStore();
     const productKey = computed(() => `${props.categorySlug}:${props.productSlug}`);
