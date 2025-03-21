f29ef82ff03a2f51f4227b7719b2ea93be3a2593
Allan <allancheboiw0@gmail.com>
2025-03-21 23:24:57 +0800

    add main layout to orders page

--- frontend/vue-project/src/views/Orders.vue
+++ frontend/vue-project/src/views/Orders.vue
@@ -1,24 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1>Your Orders</h1>
-    <div v-if="!store.isAuthenticated">Please login to view your orders</div>
-    <div v-else>
-      <div v-if="store.loading.orders">Loading...</div>
-      <div v-else-if="store.error.orders">{{ store.error.orders }}</div>
-      <div v-else-if="store.orders.length">
-        <div v-for="order in store.orders" :key="order.id">
-          <p>Order #{{ order.id }} - {{ order.quantity }} x {{ order.product_name }}</p>
-          <button v-if="order.delivery_status === 'processing'" @click="cancelOrder(order.id)">Cancel</button>
+  <MainLayout>
+    <div>
+      <h1>Your Orders</h1>
+      <div v-if="!store.isAuthenticated">Please login to view your orders</div>
+      <div v-else>
+        <div v-if="store.loading.orders">Loading...</div>
+        <div v-else-if="store.error.orders">{{ store.error.orders }}</div>
+        <div v-else-if="store.orders.length">
+          <div v-for="order in store.orders" :key="order.id">
+            <p>Order #{{ order.id }} - {{ order.quantity }} x {{ order.product_name }}</p>
+            <button v-if="order.delivery_status === 'processing'" @click="cancelOrder(order.id)">Cancel</button>
+          </div>
         </div>
+        <div v-else>No orders</div>
       </div>
-      <div v-else>No orders</div>
     </div>
-  </div>
+  </MainLayout>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useEcommerceStore } from '@/stores/ecommerce';
+import MainLayout from "@/components/navigation/MainLayout.vue";
 import api from '@/services/api';
 
 // Move the store initialization inside the setup
